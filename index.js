e14449eaf3686f77ffad52a04a43e4297ac81cd3
Tunde Jinadu <bjinad@yahoo.com>
2020-03-23 17:31:06 +0100

    firstJavaTask

---
+++ index.js
@@ -0,0 +1,10 @@
+//Startdotng First JS Task    
+let name = 'Tunde';
+console.log('My name is: '+name);
+
+let courses = ['UI/UX', 'HTML', 'CSS', 'JS'];
+console.log(courses);
+
+for (let i = 0; i <= 200; i++){
+    if (i % 2 == 0) console.log(i)
+}
